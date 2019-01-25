e81ec5847e280a729f8c4168ec3de3b1c753927a
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-01-25 15:29:06 -0800

    added button

---
+++ src/SideDrawer/SideDrawer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './SideDrawer.css'
+
+
+const sideDrawer = props =>(
+
+    <nav className = "side-drawer">
+    <ul>
+    <li ><a href = "/">This is test</a></li> 
+    <li ><a href = "/">This is test</a></li> 
+    </ul>
+</nav>
+)
+
+export default sideDrawer;
