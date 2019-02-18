27b6b556eafe7ad1f94a34ea9919ee3e38ab22e6
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-02-18 13:28:14 -0800

    need response

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './footer.css'
-import {Row, Col} from 'reactstrap'
+import {Row, Col, InputGroup, Button, InputGroupAddon, Input} from 'reactstrap'
 
+
+let icons = ['fab fa-facebook-f', 'fab fa-twitter', 'fab fa-instagram', 'fab fa-pinterest']
 
 const Footer = props =>(
 
     <div className = 'footer'>
     <Row  className = "sub">
-    <Col md = '3'>
+    <Col md = '4'>
         <ul>
             <li>FLORAL SUBCRIPTIONS</li>
             <li>WEDDINGS</li>
@@ -20,8 +22,22 @@ const Footer = props =>(
             <li>CONTACT</li>
         </ul>
           </Col>
-          <Col md ='2'>
-              <h1>Carosel</h1>
+          <Col md ='2' >
+          <div className = 'social'>
+           {icons.map(icons => 
+            <i className = {icons} social ></i>
+           )}
+           </div>
+          </Col>
+          <Col md = '6'>
+          <div className = 'form'>
+          <InputGroup >
+          <Input placeholder = "Subscribe" />
+            <InputGroupAddon addontype = "append">
+                <Button color = 'secondary'>Submit</Button>
+            </InputGroupAddon>
+          </InputGroup>
+          </div>
           </Col>
         </Row>
     </div>
